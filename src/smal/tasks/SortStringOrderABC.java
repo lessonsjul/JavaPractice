467e691de79dd5d76ccc7148213ad02799677ea1
lessonsjul <julieee8909@gmail.com>
2016-02-07 21:30:30 +0200

    Split one main method at several least methods

--- src/smal/tasks/SortStringOrderABC.java
+++ src/smal/tasks/SortStringOrderABC.java
@@ -8,42 +8,65 @@ public class SortStringOrderABC {
 
     public static void main(String[] args) {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String filename = getFileName();
+
+        if(filename != null){
+            String[] content= getFileContent(filename);
+
+            sortStringOrderABC(content);
+
+            writeContentIntoTheFile(filename,content);
+        }else{
+            System.out.println("Не введено имя файла");
+        }
+    }
+
+    private static String getFileName() {
 
         String filename = null;
         try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
             filename = reader.readLine();
-            System.out.println(filename);
+            reader.close();
         } catch (IOException e) {
-            System.out.println("We cant read you file");
+            System.out.println("We can't read name your file");
         }
 
-        String[] content= null;
+        return filename;
+    }
+
+    private static String[] getFileContent(String fileName){
+        String[] content = null;
         try {
-            FileInputStream fio = new FileInputStream(filename);
+            FileInputStream fio = new FileInputStream(fileName);
             byte[] buff = new byte[fio.available()];
             fio.read(buff);
             String contentString = new String(buff);
             content = contentString.split("\\s");
-            Arrays.sort(content);
-            System.out.println(Arrays.toString(content));
             fio.close();
         } catch (IOException e) {
-            System.out.printf("Please, check name and existing of file!");
+            System.out.println("Please, check name and existing of file!");
         }
 
+        return content;
+    }
+
+    private static void sortStringOrderABC(String[] content){
+        Arrays.sort(content);
+    }
+
+    private static void writeContentIntoTheFile(String fileName, String[] content){
         try {
-            FileOutputStream fos = new FileOutputStream(new File("sorted_" + filename));
+            FileOutputStream fos = new FileOutputStream(new File("sorted_" + fileName));
+
             String outputcontent = Arrays.stream(content).collect(Collectors.joining("\n"));
 
             fos.write(outputcontent.getBytes());
             fos.flush();
             fos.close();
-
-            System.out.println(outputcontent);
         } catch (IOException e) {
             System.out.println("We have problem with file and his writable");
         }
-
     }
 }
